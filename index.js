983189d65f5af91e166a8b522ab3ab7f5d1bf40b
tobias <tobias.omondi@student.moringaschool.com>
2023-07-12 20:30:42 +0300

    my-assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let num1 =31;
+let num2 =2;
+ if (typeof num1==='number'&& typeof num2==='number') {
+    let multiply = num1 * num2
+    
+ }
+ let random =8;
+ Math.random ();
+
+ let num3 =8;
+ let num4 =2;
+  
+   let mod =num3 / num4;
+ 
+   let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
